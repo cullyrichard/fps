627727ae121d34a0497f3396fd69395eb014ec46
Cully Richard <cully.richard@maine.edu>
2026-02-18 10:26:01 -0500

    Fix formatting issues and remove boilerplate comments

--- fpsfnlib.asm
+++ fpsfnlib.asm
@@ -1,5 +1,3 @@
-boilerplate FPS-100 template loading MD:
-```
         ; Store increment load MA with function
         NIOP 054        ;reset
 start:  NIOS 054        ;set busy
@@ -14,6 +12,7 @@ start:  NIOS 054        ;set busy
         JSR read_LT     ;read LT
         HALT            ;examine AC1
         JMP start
+
 test_MA:0000010         ;MA address
 FN_MA_D:0001102         ;FN_MA_D command
 FN_MA_E:0002102         ;FN_MA_E command
@@ -27,6 +26,7 @@ load_FN:STA 3,FN_RET
         DOB 0,054
         LDA 1,SAC1
         JMP @FN_RET
+
 ;AC0 is SR
 load_SR:STA 3,FN_RET
         STA 1,SAC1
@@ -35,14 +35,16 @@ load_SR:STA 3,FN_RET
         DOB 0,054
         LDA 1,SAC1
         JMP @FN_RET
+
 ;AC0 is LT
 read_LT:STA 3,FN_RET
         STA 1,SAC1
-        LDA 1,LT_r
+        LDA 1,LT_ r 
         DOA 1,054
         DIB 0,054
         LDA 1,SAC1
         JMP @FN_RET
+
 ;runs AP
 AP_RUN: STA 3,FN_RET2
         STA 0,SAC0
@@ -50,6 +52,7 @@ AP_RUN: STA 3,FN_RET2
         JSR load_FN
         LDA 0,SAC0
         JMP @FN_RET2
+
 ;stops AP
 AP_STOP:STA 3,FN_RET2
         STA 0,SAC0
@@ -57,6 +60,7 @@ AP_STOP:STA 3,FN_RET2
         JSR load_FN
         LDA 0,SAC0
         JMP @FN_RET2
+
 ;steps AP
 AP_STEP:STA 3,FN_RET2
         STA 0,SAC0
@@ -64,14 +68,17 @@ AP_STEP:STA 3,FN_RET2
         JSR load_FN
         LDA 0,SAC0
         JMP @FN_RET2
+
 FN_RET2:0
 FN_RET: 0
+
 SAC1:   0
 SAC0:   0
+
 SR_w:   0021031
 FN_w:   0022031
 LT_r:   0023030
+
 FN_STOP:0100000
 FN_RUN: 0040000
 FN_STEP:0010000
-```
