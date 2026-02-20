afd68cfbd8e24a6b0802e1b9daa615fc3e0b98bd
Connor Wood <connorwood71@gmail.com>
2026-02-20 12:56:50 +0000

    Remove defines
    Novae do not have immediate loads, everything must reside in memory. Move all required constants to memory, instead of constructing them using an immediate load and OR logic

--- test_fps_prog.asm
+++ test_fps_prog.asm
@@ -1,35 +1,6 @@
 	;; DEFINES SECTION
 	#define FPU 054
 
-	#define FN_STOP		0100000
-	#define FN_START 	0040000
-	#define FN_CONT		0020000
-	#define FN_STEP		0010000
-	#define FN_RESET	0004000
-	#define FN_EXAM		0002000
-	#define FN_DEP		0001000
-	#define FN_BREAK	0000400
-
-	#define FN_INC3		0000300
-	#define FN_INC2		0000200
-	#define FN_INC1		0000100
-	#define FN_NOINC	0000000
-
-	#define FN_OFFS3	0000060
-	#define FN_OFFS2	0000040
-	#define FN_OFFS1	0000020
-	#define FN_OFFS0	0000000
-
-	#define FN_REGPS	0000000
-	#define FN_REGMA	0000002
-	#define FN_REGTMA	0000003
-	#define FN_REGMD	0000005
-
-	#define CMD_WTSR	0021031
-	#define CMD_WTFN	0022031
-	#define CMD_RDFN	0022030
-	#define CMD_LTRD	0023030
-
 	JMP reset
 
 	;; DATA SECTION
@@ -38,6 +9,21 @@ load_addr:		0000200
 program_ptr:		program
 program_end_ptr:	program_end
 stop_bit:		FN_STOP
+
+fn_load_tma:		0001002
+fn_load_ps:		0001000
+fn_load_inc_ps:		0001100
+fn_start:		0040000
+fn_stop:		0100000
+fn_examine_regps:	0002000
+fn_examine_regps_o1:	0002020
+fn_examine_regps_o2:	0002040
+fn_examine_regps_o3:	0002060
+
+cmd_wtsr:		0021031
+cmd_wtfn:		0022031
+cmd_rdfn:		0022030
+cmd_rdlt:		0023030
 
 program:
 	000003
@@ -87,13 +73,13 @@ start:	NIOS FPU		; Set the FPU to busy
 
 	;; First, set the switches to the address in question
 	LDA 0,load_addr
-	LDA 1,CMD_WTSR
+	LDA 1,cmd_wtsr
 	DOA 1,FPU
 	DOB 0,FPU
 
 	;; Load address into TMA
-	LDA 0,FN_DEP|FN_REGTMA
-	LRA 1,CMD_WTFN
+	LDA 0,fn_load_tma
+	LRA 1,cmd_wtfn
 	DOA 1,FPU
 	DOB 0,FPU
 
@@ -102,12 +88,12 @@ start:	NIOS FPU		; Set the FPU to busy
 
 	;; Prime the pump - initial deposit must be without increment
 	LDA 0,0,2		; Indirectly load from the pointer in AC2
-	LDA 1,CMD_WTSR
+	LDA 1,cmd_wtsr
 	DOA 1,FPU
 	DOB 0,FPU
 
-	LDA 0,FN_DEP|FN_INC0|FN_OFFS0|FN_REGPS
-	LDA 1,CMD_WTFN
+	LDA 0,fn_load_ps
+	LDA 1,cmd_wtfn
 	DOA 1,FPU
 	DOB 0,FPU
 
@@ -115,12 +101,12 @@ start:	NIOS FPU		; Set the FPU to busy
 
 	;;  Load the program onto the FPU
 loadpg:	LDA 0,0,2		; Indirectly load from the pointer in AC2
-	LDA 1,CMD_WTSR
+	LDA 1,cmd_wtsr
 	DOA 1,FPU
 	DOB 0,FPU
 
-	LDA 0,FN_DEP|FN_INC1|FN_OFFS0|FN_REGPS
-	LDA 1,CMD_WTFN
+	LDA 0,fn_load_inc_ps
+	LDA 1,cmd_wtfn
 	DOA 1,FPU
 	DOB 0,FPU
 
@@ -130,77 +116,77 @@ loadpg:	LDA 0,0,2		; Indirectly load from the pointer in AC2
 
 	;; Start the FPU, at the address in SR
 	LDA 0,load_addr
-	LDA 1,CMD_WTSR
+	LDA 1,cmd_wtsr
 	DOA 1,FPU
 	DOB 0,FPU
 
-	LDA 0,FN_START
-	LDA 1,CMD_WTFN
+	LDA 0,fn_start
+	LDA 1,cmd_wtfn
 	DOA 1,FPU
 	DOB 0,FPU
 
 	;; Wait until the FPU is finished
 waitstop:
-	LDA 1,CMD_RDFN
+	LDA 1,cmd_rdfn
 	DOA 1,FPU
 	DIB 0,FPU
 
-	LDA 1,FN_STOP
+	LDA 1,fn_stop
 	AND# 0,1,SNR
 	JMP waitstop
 
 	;; Read back the results
 	LDA 0,results_addr
-	LRA 1,CMD_WTSR
+	LRA 1,cmd_wtsr
 	DOA 1,FPU
 	DOB 0,FPU
 
 	;; Load address into TMA
-	LDA 0,FN_DEP|FN_REGTMA
-	LRA 1,CMD_WTFN
+	LDA 0,fn_load_tma
+	LRA 1,cmd_rtfn
 	DOA 1,FPU
 	DOB 0,FPU
 
 	;; First word
-	LDA 0,FN_EXAM|FN_REGPS
-	LRA 1,CMD_WTFN
+	LDA 0,fn_examine_regps
+	LRA 1,cmd_rtfn
 	DOA 1,FPU
 	DOB 0,FPU
 
-	LDA 1,CMD_RDLT
+	LDA 1,cmd_rdlt
 	DOA 1,FPU
 	DIB 0,FPU
 	HALT
 
 	;; Second word
-	LDA 0,FN_EXAM|FN_REGPS|FN_OFFS1
-	LRA 1,CMD_WTFN
+	LDA 0,fn_examine_regps_o1
+	LRA 1,cmd_wtfn
 	DOA 1,FPU
 	DOB 0,FPU
 
-	LDA 1,CMD_RDLT
+	LDA 1,cmd_rdlt
 	DOA 1,FPU
 	DIB 0,FPU
 	HALT
 
 	;; Third word
-	LDA 0,FN_EXAM|FN_REGPS|FN_OFFS2
-	LRA 1,CMD_WTFN
+	LDA 0,fn_examine_regps_o2
+	LRA 1,cmd_wtfn
 	DOA 1,FPU
 	DOB 0,FPU
 
-	LDA 1,CMD_RDLT
+	LDA 1,cmd_rdlt
 	DOA 1,FPU
 	DIB 0,FPU
 	HALT
 
 	;; Fourth word
-	LDA 0,FN_EXAM|FN_REGPS|FN_OFFS3
-	LRA 1,CMD_WTFN
+	LDA 0,fn_examine_regps_o3
+	LRA 1,cmd_wtfn
 	DOA 1,FPU
 	DOB 0,FPU
 
-	LDA 1,CMD_RDLT
+	LDA 1,cmd_rdlt
 	DOA 1,FPU
 	DIB 0,FPU
 	HALT
